90eed9f774d682e8db851e134d165dc8e34e06e7
pouryawJs <pourya.bu@gmail.com>
2024-02-03 21:35:35 +0330

    add ejs template engine to project

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
+// default settings
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// static
+app.use("/css", express.static(path.join(__dirname, "public", "css")));
+app.use("/js", express.static(path.join(__dirname, "public", "js")));
+// template engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+//
 
 module.exports = app;
