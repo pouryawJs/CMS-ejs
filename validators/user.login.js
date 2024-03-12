d33b2df10849de5572212a7897f8529a1cdee32a
pouryawJs <pourya.bu@gmail.com>
2024-03-12 20:56:37 +0330

    fix validation messages

--- validators/user.login.js
+++ validators/user.login.js
@@ -5,15 +5,15 @@ const loginValidator = () => {
         body("username")
             .trim()
             .isLength({ min: 3, max: 12 })
-            .withMessage("یوزر نیم باید بین 3 الی 12 کارکتر باشد")
+            .withMessage("username must be 3-12 character")
             .notEmpty()
-            .withMessage("یوزرنیم اجباریست"),
+            .withMessage("username is require"),
         body("password")
             .trim()
             .isLength({ min: 8, max: 36 })
-            .withMessage("رمز عبور باید بین 8 تا 36 کارکتر باشد")
+            .withMessage("password must be 8-36 character")
             .notEmpty()
-            .withMessage("رمز اجباریست"),
+            .withMessage("password is require"),
     ];
 };
 
--- validators/user.register.js
+++ validators/user.register.js
@@ -5,27 +5,27 @@ const registerValidator = () => {
         body("username")
             .trim()
             .isLength({ min: 3, max: 12 })
-            .withMessage("یوزر نیم باید بین 3 الی 12 کارکتر باشد")
+            .withMessage("username must be 3-12 character")
             .notEmpty()
-            .withMessage("یوزرنیم اجباریست"),
+            .withMessage("username is require"),
         body("firstname")
             .trim()
             .isLength({ min: 3, max: 24 })
-            .withMessage("نام باید بین 3 تا 24 کارکتر باشد")
+            .withMessage("firstname must be 3-24 character")
             .notEmpty()
-            .withMessage("نام اجباریست"),
+            .withMessage("firstname is require"),
         body("lastname")
             .trim()
             .isLength({ min: 3, max: 24 })
-            .withMessage("نام خانوادگی باید بین 3 تا 24 کارکتر باشد")
+            .withMessage("lastname must be 3-24 character")
             .notEmpty()
-            .withMessage("نام خانوادگی اجباریست"),
+            .withMessage("lastname is require"),
         body("password")
             .trim()
             .isLength({ min: 8, max: 36 })
-            .withMessage("رمز عبور باید بین 8 تا 36 کارکتر باشد")
+            .withMessage("password must be 8-36 character")
             .notEmpty()
-            .withMessage("رمز اجباریست"),
+            .withMessage("password is require"),
     ];
 };
 
